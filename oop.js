5e5f92deb5e7687cb8df9deaa3875529daa51aa4
Enogwe Victor <victor.enogwe@gmail.com>
2016-10-17 19:48:14 +0100

    create oop.js
    add inheritance polymorphism

---
+++ oop.js
@@ -0,0 +1,25 @@
+function Building(rooms, color, storey){
+  //property of the class
+  this.name = "House";
+  this.rooms = rooms;
+  this.color = color;
+  this.storey = storey;
+}
+
+Building.prototype.build = function(){
+    return "A " + this.name + ", with " + this.rooms + " rooms" + " painted " + this.color + " having "  + this.storey + " stories"
+}
+
+
+function Bungalow(rooms, color, storey){
+	this.name = "Bungalow";
+	this.rooms = rooms;
+    this.color = color;
+    this.storey = storey;
+}
+
+//inheritance 
+Bungalow.prototype = new Building();
+
+var Bunga = new Bungalow(3, "red", "3");
+console.log(Bunga.build())
