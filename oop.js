33c65033e34359a76008fa4a281024c864d193f6
Enogwe Victor <victor.enogwe@gmail.com>
2016-10-19 23:05:41 +0100

    update OOP construct

--- oop.js
+++ oop.js
@@ -10,17 +10,39 @@ Building.prototype.build = function(){
     return "A " + this.name + ", with " + this.rooms + " rooms" + " painted " + this.color + " having "  + this.storey + " stories"
 }
 
-
-function Bungalow(rooms, color, storey){
-    //polymorhism
-    this.name = "Bungalow";
-    this.rooms = rooms;
-    this.color = color;
-    this.storey = storey;
+Building.prototype.demolish = function(vehicle){
+	if(arguments.length === 0){
+		return "please enter a demolition vehicle"
+	}
+	return "demolish " + this.rooms + " rooms with a " + vehicle;
 }
 
+function Bungalow(rooms, color, storey){
+	Building.call(this, rooms, color, storey)
+    //polymorphism
+    this.name = "Bungalow"; //public property
+    var toilet = "currently in use"; //private property
+    this.toilet = toilet;
+    //encapsulation toilet and free toilet
+    this.useToilet = function(){
+    	toilet = "toilet now freed up for use";
+    	this.toilet = toilet;
+    	return "Freeing up toilet";
+    }
+    this.furnish = function(){
+        this.tv = "Samsung"
+        return "Add, furniture, electronic gadgets and wallpapers to house";
+    }
+}
+//polymorphism
+Bungalow.prototype.demolish = function(){
+	return "demolish " + this.rooms + " with a " + (arguments[0] || "Caterpillar");
+}
 //inheritance 
 Bungalow.prototype = new Building();
+//abstraction , we do not need to know the implementation of Bungalow
 
 var Bunga = new Bungalow(3, "red", "3");
-console.log(Bunga.build())
+Bunga.furnish()
+Bunga.tv = "Panasonic";
+console.log(Bunga.toilet, Bunga.useToilet(), Bunga.toilet)
